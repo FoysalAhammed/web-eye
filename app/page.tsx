a887ca2d932903adf326722b3d882614a600fc31
FoysalAhammed <hanif.mia2233@gmail.com>
2024-06-27 08:54:40 +0600

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,23 @@
+import Navbar from '@/app/components/navbar/page'
+import Hero from '@/app/components/hero/page'
+import Feature from '@/app/components/features/page'
+import Testimonials from '@/app/components/testimonials/page'
+import Faq from '@/app/components/faqs/page'
+import Plans from '@/app/components/plans/page'
+import Cta from '@/app/components/cta/page'
+import Footer from '@/app/components/footer/page'
+
+export default function Home() {
+  return (
+    <>
+      <Navbar/>
+      <Hero/>
+      <Feature/>
+      <Testimonials/>
+      <Faq/>
+      <Plans/>
+      <Cta/>
+      <Footer/>
+    </>
+  );
+}
